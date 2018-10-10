3f9b094541c0a1ada7277526757319badeb3957b
Alfredo Luco <alfredo.luco@mavericks.cool>
2018-10-10 11:36:47 -0300

    Initial Commit

---
+++ MAVAWSSNSSubscriptor/MAVAWSSNSSubscriptor.h
@@ -0,0 +1,19 @@
+//
+//  MAVAWSSNSSubscriptor.h
+//  MAVAWSSNSSubscriptor
+//
+//  Created by Mavericks's iOS Dev on 10/10/18.
+//  Copyright © 2018 Mavericks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MAVAWSSNSSubscriptor.
+FOUNDATION_EXPORT double MAVAWSSNSSubscriptorVersionNumber;
+
+//! Project version string for MAVAWSSNSSubscriptor.
+FOUNDATION_EXPORT const unsigned char MAVAWSSNSSubscriptorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MAVAWSSNSSubscriptor/PublicHeader.h>
+
+
